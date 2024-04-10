5f7fa76b7fb3c8d6781166ea22d16cb210512e8d
lance_ali <446006871@qq.com>
2024-04-11 01:44:09 +0800

    first commit

---
+++ helplerx/snowflake.go
@@ -0,0 +1,14 @@
+package helplerx
+
+import (
+	"github.com/bwmarrin/snowflake"
+)
+
+func GenerateSnowFlakeID() (int64, error) {
+	node, err := snowflake.NewNode(1)
+	if err != nil {
+		return 0, err
+	}
+
+	return node.Generate().Int64(), nil
+}
