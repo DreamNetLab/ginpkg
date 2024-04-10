399f4c865abc266084999d1095eea9c45c2bfd14
lance_ali <446006871@qq.com>
2024-04-11 01:03:11 +0800

    first commit

--- redisx/redis.go
+++ redisx/redis.go
@@ -19,7 +19,7 @@ type ClientConfig struct {
 }
 
 type RedisxClient struct {
-	Rdb redis.UniversalClient
+	rdb redis.UniversalClient
 }
 
 func Setup(config *ClientConfig) (*RedisxClient, error) {
@@ -33,14 +33,14 @@ func Setup(config *ClientConfig) (*RedisxClient, error) {
 	}
 
 	redisClient := &RedisxClient{
-		Rdb: redis.NewUniversalClient(&opts),
+		rdb: redis.NewUniversalClient(&opts),
 	}
 
 	ctx := context.Background()
 
-	_, err := redisClient.Rdb.Ping(ctx).Result()
+	_, err := redisClient.rdb.Ping(ctx).Result()
 	if err != nil {
-		_ = redisClient.Rdb.Close()
+		_ = redisClient.rdb.Close()
 		return nil, err
 	}
 
@@ -51,7 +51,7 @@ func (rc *RedisxClient) SetString(key string, data string, expire int) error {
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
 	defer cancel()
 
-	err := rc.Rdb.Set(ctx, key, data, time.Duration(expire)*time.Second).Err()
+	err := rc.rdb.Set(ctx, key, data, time.Duration(expire)*time.Second).Err()
 	if err != nil {
 		return err
 	}
@@ -63,7 +63,7 @@ func (rc *RedisxClient) SetStringNx(key string, data any, expire int) error {
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
 	defer cancel()
 
-	err := rc.Rdb.SetNX(ctx, key, data, time.Duration(expire)*time.Second).Err()
+	err := rc.rdb.SetNX(ctx, key, data, time.Duration(expire)*time.Second).Err()
 	if err != nil {
 		return err
 	}
@@ -75,7 +75,7 @@ func (rc *RedisxClient) Exist(key string) bool {
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
 	defer cancel()
 
-	_, err := rc.Rdb.Get(ctx, key).Result()
+	_, err := rc.rdb.Get(ctx, key).Result()
 
 	return err == nil
 }
@@ -84,7 +84,7 @@ func (rc *RedisxClient) Get(key string) ([]byte, error) {
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
 	defer cancel()
 
-	value, err := rc.Rdb.Get(ctx, key).Result()
+	value, err := rc.rdb.Get(ctx, key).Result()
 	if errors.Is(err, redis.Nil) {
 		return nil, nil
 	} else if err != nil {
@@ -98,7 +98,7 @@ func (rc *RedisxClient) Delete(key string) error {
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
 	defer cancel()
 
-	_, err := rc.Rdb.Del(ctx, key).Result()
+	_, err := rc.rdb.Del(ctx, key).Result()
 	if err != nil {
 		return err
 	}
